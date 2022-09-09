c4faf15fd4079ae8a2346694f9f8c102679d6f00
Frerich Raabe <frerich.raabe@gmail.com>
2022-09-09 12:28:05 +0200

    Add infrastructure for building proper Hex packages

--- mix.exs
+++ mix.exs
@@ -4,11 +4,19 @@ defmodule PhoenixLiveDebugConsole.MixProject do
   def project do
     [
       app: :phoenix_live_debug_console,
+      description: description(),
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      aliases: aliases()
+      aliases: aliases(),
+      source_url: github_url(),
+      name: "Phoenix LiveDebugConsole",
+      docs: [
+        extras: ["README.md", "LICENSE.md"],
+        main: "readme"
+      ],
+      package: package()
     ]
   end
 
@@ -33,6 +41,22 @@ defmodule PhoenixLiveDebugConsole.MixProject do
   defp aliases do
     [
       "assets.deploy": ["esbuild default --minify"]
+    ]
+  end
+
+  defp description do
+    "An extension to the Phoenix error view, adding a fully-functional IEx terminal to it."
+  end
+
+  defp github_url do
+    "https://github.com/frerich/phoenix_live_debug_console"
+  end
+
+  defp package do
+    [
+      files: ~w(dist lib .formatter.exs mix.exs README.md LICENSE.md),
+      licenses: ["MIT"],
+      links: %{"GitHub" => github_url()}
     ]
   end
 end
