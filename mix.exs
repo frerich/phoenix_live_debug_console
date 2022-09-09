ec8b392292c7cc82ce5606d5e3e17feae9a4730c
Frerich Raabe <frerich.raabe@gmail.com>
2022-09-09 12:35:49 +0200

    Bump version number for 'mix docs' fix

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule PhoenixLiveDebugConsole.MixProject do
     [
       app: :phoenix_live_debug_console,
       description: description(),
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
