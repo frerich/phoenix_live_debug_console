ed0f367944c0bb0e35baaa3f9b741b764b62d985
Frerich Raabe <frerich.raabe@gmail.com>
2022-09-09 08:34:00 +0200

    Drop bogus test

--- test/phoenix_live_debug_console_test.exs
+++ test/phoenix_live_debug_console_test.exs
@@ -1,8 +1,4 @@
 defmodule PhoenixLiveDebugConsoleTest do
   use ExUnit.Case
   doctest PhoenixLiveDebugConsole
-
-  test "greets the world" do
-    assert PhoenixLiveDebugConsole.hello() == :world
-  end
 end
