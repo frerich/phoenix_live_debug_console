21562c4a9fe62d65f1230467551ef859f5980255
Frerich Raabe <frerich.raabe@gmail.com>
2022-09-09 10:03:51 +0200

    Reduce required Elixir version to 1.12.x
    1.14 is a little too recent, it's not widely available nor needed by us.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule PhoenixLiveDebugConsole.MixProject do
     [
       app: :phoenix_live_debug_console,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       aliases: aliases()
